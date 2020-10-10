de2401926df87e09b411595ecb64caf2fd1e3255
Shivanirao2000 <shivanirao2000@gmail.com>
2020-10-10 23:00:23 +0530

    Updates

---
+++ precache-manifest.6c425015fcb437162fef610f09a1ae4a.js
@@ -0,0 +1,110 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7cf6bf65440d3a0af30949763a4253c",
+    "url": "/FrontEndUI/index.html"
+  },
+  {
+    "revision": "87d9b9357029882331b1",
+    "url": "/FrontEndUI/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "b32db45321442d961154",
+    "url": "/FrontEndUI/static/css/main.8927284f.chunk.css"
+  },
+  {
+    "revision": "87d9b9357029882331b1",
+    "url": "/FrontEndUI/static/js/2.7c45ec87.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/FrontEndUI/static/js/2.7c45ec87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b32db45321442d961154",
+    "url": "/FrontEndUI/static/js/main.ec5be8f8.chunk.js"
+  },
+  {
+    "revision": "02a8c66bd532aea57e06",
+    "url": "/FrontEndUI/static/js/runtime-main.b4dc0651.js"
+  },
+  {
+    "revision": "8d83a19afe0cc7102cc85282b6caa249",
+    "url": "/FrontEndUI/static/media/2nd @0.75x.8d83a19a.png"
+  },
+  {
+    "revision": "10cc915c456031aec6be26ce0befb62d",
+    "url": "/FrontEndUI/static/media/2nd bubble.10cc915c.svg"
+  },
+  {
+    "revision": "580b1de471aee801118367ec09c75aa5",
+    "url": "/FrontEndUI/static/media/5.580b1de4.svg"
+  },
+  {
+    "revision": "dc61567e60cf990ba7970071a7b62f72",
+    "url": "/FrontEndUI/static/media/board for icons.dc61567e.svg"
+  },
+  {
+    "revision": "0c86dc8c494e6a05407262dcce9ac03b",
+    "url": "/FrontEndUI/static/media/bubble (what  can we surve for you).0c86dc8c.svg"
+  },
+  {
+    "revision": "29c4cee053df0f63c03100a2ef195381",
+    "url": "/FrontEndUI/static/media/clients bubble.29c4cee0.svg"
+  },
+  {
+    "revision": "cc0f755f9e392f0ac134dfac58cc8ea9",
+    "url": "/FrontEndUI/static/media/footer logo.cc0f755f.svg"
+  },
+  {
+    "revision": "4cef209c49303e5a554cbf049eb356ef",
+    "url": "/FrontEndUI/static/media/icons (1).4cef209c.svg"
+  },
+  {
+    "revision": "f287d09cd08b86104f8067874d197fce",
+    "url": "/FrontEndUI/static/media/icons (2).f287d09c.svg"
+  },
+  {
+    "revision": "fa751f454916ebf826573fb08c2f1e49",
+    "url": "/FrontEndUI/static/media/icons (3).fa751f45.svg"
+  },
+  {
+    "revision": "81795ba50f73cd082261cec456497418",
+    "url": "/FrontEndUI/static/media/icons (4).81795ba5.svg"
+  },
+  {
+    "revision": "46b1f13b2d5f565bbc03d06db6adfd7a",
+    "url": "/FrontEndUI/static/media/icons (5).46b1f13b.svg"
+  },
+  {
+    "revision": "115f6bd5a7dca795d8b97eadbaf9efce",
+    "url": "/FrontEndUI/static/media/icons (6).115f6bd5.svg"
+  },
+  {
+    "revision": "c41f124e3a6b13af53e1e1cf8a072397",
+    "url": "/FrontEndUI/static/media/landing page banner main.c41f124e.svg"
+  },
+  {
+    "revision": "5eab57366fcb2cbcb349d111a84c0e9b",
+    "url": "/FrontEndUI/static/media/landing page main pic@0.75x.5eab5736.png"
+  },
+  {
+    "revision": "70d40dbb3c89434229007c1e3808949f",
+    "url": "/FrontEndUI/static/media/last_team@0.75x.70d40dbb.png"
+  },
+  {
+    "revision": "2493f667d84cf1811e2b5098c761d315",
+    "url": "/FrontEndUI/static/media/last_team_banner.2493f667.svg"
+  },
+  {
+    "revision": "d5d77caa747affbe13a31bec8e2cd501",
+    "url": "/FrontEndUI/static/media/team 2@0.75x.d5d77caa.png"
+  },
+  {
+    "revision": "57a654c2173ef41bde53bb3af4fe38c0",
+    "url": "/FrontEndUI/static/media/team @0.75x.57a654c2.png"
+  },
+  {
+    "revision": "d60d37bfc0bc64e8c050d4010f44f152",
+    "url": "/FrontEndUI/static/media/team bubble.d60d37bf.svg"
+  }
+]);
